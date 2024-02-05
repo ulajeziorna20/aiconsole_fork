21f3c1ed9294ceba22d42027f87e051e44b50a93
vlad <vlad@malinovski.org>
2024-02-05 20:00:31 +0100

    * feat(electron): add support for zip maker in electron forge config * feat(electron): add cross-zip and electron-forge/maker-zip dependencies

--- electron/forge.config.cjs
+++ electron/forge.config.cjs
@@ -64,6 +64,14 @@ module.exports = {
       },
     },
     {
+      name: '@electron-forge/maker-zip',
+      config: {
+        icon: './assets/icon.png',
+        name: 'AIConsole',
+        options: {},
+      },
+    },
+    {
       name: '@electron-forge/maker-deb',
       config: {
         options: {
