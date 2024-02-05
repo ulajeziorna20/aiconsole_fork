81ff009c6dc78262e6af37098d2b28db0821bd2c
AGasior <aleksander.gasior@10clouds.com>
2024-02-05 20:41:44 +0100

    feat: refactor chatOptions

--- frontend/src/components/editables/assets/ChatOptions.tsx
+++ frontend/src/components/editables/assets/ChatOptions.tsx
@@ -14,299 +14,137 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { useDebounceCallback } from '@mantine/hooks';
-import * as Collapsible from '@radix-ui/react-collapsible';
-import { Content, DropdownMenu, Item, Trigger } from '@radix-ui/react-dropdown-menu';
-import { ArrowDownLeftSquare, ArrowUpRightSquare, ChevronDown, ChevronUp, X } from 'lucide-react';
-import { useEffect, useState } from 'react';
-
-import Checkbox from '@/components/common/Checkbox';
-import { Icon } from '@/components/common/icons/Icon';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { Pin, Plus } from 'lucide-react';
+import { cn } from '@/utils/common/cn';
 import { useChatStore } from '@/store/editables/chat/useChatStore';
 import { useEditablesStore } from '@/store/editables/useEditablesStore';
-import { Agent, Material } from '@/types/editables/assetTypes';
-import { cn } from '@/utils/common/cn';
+import { Icon } from '@/components/common/icons/Icon';
+import { useClickOutside } from '@/utils/common/useClickOutside';
 import { getEditableObjectIcon } from '@/utils/editables/getEditableObjectIcon';
-
+import { Agent, Material } from '@/types/editables/assetTypes';
 import { AgentAvatar } from '../chat/AgentAvatar';
-import Autocomplete from './Autocomplete';
-import { ChatAPI } from '@/api/api/ChatAPI';
 
-const ChatOptions = () => {
+type ChatOptionsProps = {
+  onSelectAgentId: (id: string) => void;
+  handleMaterialSelect: (material: Material) => void;
+  materialsOptions: Material[];
+};
+
+const ChatOption = ({
+  option,
+  selectOption,
+  disabled,
+}: {
+  option: Material;
+  selectOption: (option: Material) => void;
+  disabled: boolean;
+}) => {
+  const OptionIcon = getEditableObjectIcon(option);
+
+  return (
+    <div className="flex justify-between items-center max-w-full w-max gap-3 hover:bg-gray-600 px-2 py-1 rounded-[8px] max-h-[32px] group">
+      <Icon icon={OptionIcon} className="w-6 h-6 min-h-6 min-w-6 text-material" />
+      <p className="flex-1 truncate font-normal text-sm text-gray-400 group-hover:text-white">{option.name}</p>
+      <button onClick={() => selectOption(option)} disabled={disabled}>
+        <Icon icon={Plus} className="group-hover:!text-white" />
+      </button>
+    </div>
+  );
+};
+
+const ChatOptions = ({ onSelectAgentId, handleMaterialSelect, materialsOptions }: ChatOptionsProps) => {
   const chat = useChatStore((state) => state.chat);
   const agents = useEditablesStore((state) => state.agents);
-  const materials = useEditablesStore((state) => state.materials);
-  const setChat = useChatStore((state) => state.setChat);
+  const [inputValue, setInputValue] = useState('');
+  const [filteredMaterialOptions, setFilteredMaterialOptions] = useState<Material[]>([]);
+  const [filteredAgentsOptions, setFilteredAgentsOptions] = useState<Agent[]>(agents);
+  const wrapperRef = useRef<HTMLDivElement>(null);
   const isChatLoading = useChatStore((state) => state.isChatLoading);
-  const isChatOptionsExpanded = useChatStore((state) => state.isChatOptionsExpanded);
-  const setIsChatOptionsExpanded = useChatStore((state) => state.setIsChatOptionsExpanded);
-
-  const [selectingForcedMaterials, setSelectingForcedMaterials] = useState(false);
-
-  const [materialsOptions, setMaterialsOptions] = useState<Material[]>([]);
-  const [selectedAgentId, setSelectedAgentId] = useState<string>('');
-  const [chosenMaterials, setChosenMaterials] = useState<Material[]>([]);
-  const [allowExtraMaterials, setAllowExtraMaterials] = useState<boolean>(true);
-  const materialsIds = chosenMaterials.map((material) => material.id);
 
   useEffect(() => {
-    setSelectedAgentId('');
-    setChosenMaterials([]);
-    setAllowExtraMaterials(true);
-  }, [chat?.id]);
+    setFilteredMaterialOptions(materialsOptions);
+  }, [materialsOptions]);
 
-  useEffect(() => {
-    if (chat?.chat_options.agent_id) {
-      setSelectedAgentId(chat?.chat_options.agent_id);
-    }
-  }, [chat?.chat_options.agent_id, chat?.id]);
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setInputValue(inputValue);
 
-  useEffect(() => {
-    const filteredMaterials = materials?.filter(({ id }) => (chat?.chat_options.materials_ids || []).includes(id));
-    if (filteredMaterials) {
-      setChosenMaterials(filteredMaterials);
-    }
-  }, [chat?.chat_options.materials_ids, materials, chat?.id]);
-
-  useEffect(() => {
-    if (chat?.chat_options) {
-      setAllowExtraMaterials(chat?.chat_options.let_ai_add_extra_materials);
-    }
-  }, [chat?.chat_options, chat?.id]);
-
-  const debounceChatUpdate = useDebounceCallback(async () => {
-    try {
-      if (chat) {
-        ChatAPI.patchChatOptions(chat?.id, {
-          agent_id: selectedAgentId,
-          materials_ids: materialsIds,
-          let_ai_add_extra_materials: allowExtraMaterials,
-        });
-
-        setChat({
-          ...chat,
-          chat_options: {
-            agent_id: selectedAgentId,
-            materials_ids: materialsIds,
-            let_ai_add_extra_materials: allowExtraMaterials,
-          },
-        });
-      }
-    } catch (error) {
-      console.error('An error occurred while updating chat options:', error);
-    }
-  }, 500);
-
-  const onSelectAgentId = (id: string) => {
-    setSelectedAgentId(id);
-    debounceChatUpdate();
+    const regex = new RegExp(`^${inputValue}`, 'i');
+    const filteredMaterialOptions = materialsOptions.filter((item) => regex.test(item.name));
+    const filteredAgentOptions = agents.filter((item) => regex.test(item.name));
+    setFilteredAgentsOptions(filteredAgentOptions);
+    setFilteredMaterialOptions(filteredMaterialOptions);
   };
 
-  const handleMaterialSelect = (material: Material) => {
-    setChosenMaterials((prev) => [...prev, material]);
-    const filteredOptions = materialsOptions.filter(({ id }) => id !== material.id);
-    setMaterialsOptions(filteredOptions);
-    debounceChatUpdate();
+  const handleClickOutside = () => {
+    setInputValue('');
   };
 
-  const removeSelectedMaterial = (id: string) => {
-    const material = chosenMaterials.find((material) => material.id === id) as Material;
-    setChosenMaterials((prev) => prev.filter(({ id }) => id !== material.id));
-    setMaterialsOptions((prev) => [...prev, material].sort((a, b) => a.name.localeCompare(b.name)));
-    debounceChatUpdate();
-  };
-
-  const changeAllowExtraMaterials = (checked: boolean) => {
-    setAllowExtraMaterials(checked);
-    debounceChatUpdate();
-  };
-
-  useEffect(() => {
-    setMaterialsOptions(materials?.filter((material) => !chosenMaterials.includes(material)) as Material[]);
-  }, [chat?.id, materials, chosenMaterials]);
+  useClickOutside(wrapperRef, handleClickOutside);
 
   if (!chat && !isChatLoading) {
     return;
   }
 
   return (
-    <div className="w-full px-5 pb-3 bg-gray-800">
-      <div className="text-gray-300 flex flex-col gap-5 flex-1">
-        <Collapsible.Root open={isChatOptionsExpanded} onOpenChange={setIsChatOptionsExpanded}>
-          <Collapsible.Trigger className="w-full pt-5">
-            <div className="w-full flex justify-between group h-6 transition ease-in-out">
-              <h3 className="text-sm">Chat options</h3>
-              <div className="hidden group-hover:block">
-                {isChatOptionsExpanded ? (
-                  <ArrowDownLeftSquare className="stroke-[1.2]" />
-                ) : (
-                  <ArrowUpRightSquare className="stroke-[1.2]" />
-                )}
-              </div>
-            </div>
-          </Collapsible.Trigger>
-          <Collapsible.Content className="CollapsibleContent">
-            <div className="pt-5 flex flex-col gap-5">
-              {!selectingForcedMaterials && (
-                <>
-                  <div className="flex flex-col gap-2.5 border-b-2 border-gray-600 pb-5">
-                    <label htmlFor="agents" className="text-xs">
-                      Selected agent
-                    </label>
-                    <AgentsDropdown
-                      agents={agents}
-                      selectedAgent={agents.find(({ id }) => id === selectedAgentId)}
-                      onSelect={onSelectAgentId}
-                    />
-                  </div>
-
-                  <div className="flex items-center gap-2.5 mt-auto border-b-2 border-gray-600 pb-5">
-                    <Checkbox
-                      id="extraMaterials"
-                      checked={allowExtraMaterials}
-                      onChange={changeAllowExtraMaterials}
-                      disabled={isChatLoading}
-                    />
-                    <label htmlFor="extraMaterials" className="text-sm">
-                      Let AI add extra materials
-                    </label>
-                  </div>
-                </>
-              )}
-
-              <div className="flex flex-col gap-2.5">
-                <label htmlFor="agents" className="text-xs">
-                  Always used materials
-                </label>
-                {!selectingForcedMaterials && (
-                  <div className="overflow-y-auto my-2">
-                    <div className="flex flex-col gap-2.5 w-full">
-                      {chosenMaterials.map((option) => (
-                        <ChatOption
-                          option={option}
-                          onRemove={removeSelectedMaterial}
-                          key={option.id}
-                          disabled={isChatLoading}
-                        />
-                      ))}
-                    </div>
-                  </div>
-                )}
-                {selectingForcedMaterials && (
-                  <div className="flex justify-between items-center max-w-full w-max gap-2.5 bg-gray-600 px-2.5 py-2 rounded-[20px]">
-                    <p className="flex-1 truncate font-normal text-xl text-material">{chosenMaterials.length}</p>
-                    <p className="flex-1 font-normal text-sm">{'Selected'}</p>
-                  </div>
-                )}
-                {selectingForcedMaterials && (
-                  <div className="h-[120px]">
-                    <Autocomplete options={materialsOptions} onOptionSelect={handleMaterialSelect} />
-                  </div>
-                )}
-                <button
-                  className="text-s my-3 items-center gap-[12px] rounded-[8px] border border-gray-500 px-[16px] py-[10px] text-gray-300 text-[16px] w-full leading-[23px] hover:border-gray-300 transition duration-200 hover:text-gray-300 disabled:hover:border-gray-500"
-                  onClick={() => setSelectingForcedMaterials(!selectingForcedMaterials)}
-                >
-                  {selectingForcedMaterials ? 'Back' : 'Add materials'}
-                </button>
-              </div>
-            </div>
-          </Collapsible.Content>
-        </Collapsible.Root>
+    <div
+      className={cn('min-h-[290px] flex items-end absolute -top-[300px] w-full max-w-[640px]', {
+        // '-top-[312px]': inputValue.length === 0,
+      })}
+    >
+      <div className="flex flex-col py-3 px-2 w-full bg-gray-800 rounded-[8px] min-h-[164px]">
+        <div className="relative flex flex-col gap-2" ref={wrapperRef}>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Search for an agent or material"
+            className="bg-transparent p-2 focus:outline-none border-gray-400 text-white w-full placeholder:text-gray-400 placeholder:text-[15px]"
+            disabled={isChatLoading}
+          />
+          <ul className="max-h-[134px] overflow-y-auto">
+            {filteredAgentsOptions.length === 0 ? (
+              <p className="text-sm p-2 text-gray-400">There is no agent with this name.</p>
+            ) : (
+              filteredAgentsOptions.map((option) => {
+                return (
+                  <li
+                    key={option.id}
+                    className={cn(
+                      'w-full overflow-hidden p-2 flex items-center cursor-pointer hover:bg-gray-600 rounded-[8px] max-h-[44px] gap-2 group',
+                    )}
+                    onClick={() => onSelectAgentId(option.id)}
+                  >
+                    <AgentAvatar agentId={option.id} title={option.name} type="extraSmall" className="mb-0 mt-0" />
+                    <h4 className="text-white ml-[4px] text-[15px]">{option.name}</h4>
+                    <span className="text-sm truncate text-gray-400">{option.usage}</span>
+                    <Icon icon={Pin} className={cn('w-4 h-4 min-h-4 min-w-4 flex-shrink-0 group-hover:!text-white')} />
+                  </li>
+                );
+              })
+            )}
+          </ul>
+          <div className="h-1 border-b border-gray-600" />
+          <div className="max-h-[76px] overflow-y-auto flex gap-2 w-full flex-wrap">
+            {materialsOptions.length === 0 && <p className="text-sm p-2 text-gray-400">There is no more materials.</p>}
+            {filteredMaterialOptions.length === 0 && materialsOptions.length !== 0 && (
+              <p className="text-sm p-2 text-gray-400">There is no material with this name.</p>
+            )}
+            {filteredMaterialOptions.length !== 0 &&
+              materialsOptions.length !== 0 &&
+              filteredMaterialOptions.map((option) => (
+                <ChatOption
+                  option={option}
+                  selectOption={handleMaterialSelect}
+                  key={option.id}
+                  disabled={isChatLoading}
+                />
+              ))}
+          </div>
+        </div>
       </div>
     </div>
-  );
-};
-
-const ChatOption = ({
-  option,
-  onRemove,
-  disabled,
-}: {
-  option: Material;
-  onRemove: (id: string) => void;
-  disabled: boolean;
-}) => {
-  const OptionIcon = getEditableObjectIcon(option);
-
-  return (
-    <div className="flex justify-between items-center max-w-full w-max gap-2.5 bg-gray-600 px-2.5 py-2 rounded-[20px]">
-      <Icon icon={OptionIcon} className="w-6 h-6 min-h-6 min-w-6 text-material" />
-      <p className="flex-1 truncate font-normal text-sm">{option.name}</p>
-      <button onClick={() => onRemove(option.id)} disabled={disabled}>
-        <Icon icon={X} />
-      </button>
-    </div>
-  );
-};
-
-type AgentsDropdownProps = {
-  agents: Agent[];
-  selectedAgent?: Agent;
-  onSelect: (agentId: string) => void;
-};
-
-const AgentsDropdown = ({ agents, selectedAgent, onSelect }: AgentsDropdownProps) => {
-  const [opened, setOpened] = useState<boolean>(false);
-  const isChatLoading = useChatStore((state) => state.isChatLoading);
-
-  return (
-    <DropdownMenu open={opened} onOpenChange={setOpened}>
-      <Trigger asChild disabled={isChatLoading}>
-        <button
-          className={cn(
-            'group flex justify-center items-center gap-[12px] rounded-[8px] border border-gray-500 px-[16px] py-[10px] text-gray-300 text-[16px] w-full leading-[23px] hover:border-gray-300 transition duration-200 hover:text-gray-300 disabled:hover:border-gray-500',
-            {
-              'rounded-b-none bg-gray-700 border-gray-800 text-gray-500': opened,
-            },
-          )}
-        >
-          {selectedAgent ? (
-            <div className="flex gap-2.5 items-center">
-              <AgentAvatar agentId={selectedAgent.id} type="extraSmall" className="!m-0" />
-              <p>{selectedAgent.name}</p>
-            </div>
-          ) : (
-            <p>AI Choice</p>
-          )}
-          <Icon
-            icon={opened ? ChevronUp : ChevronDown}
-            width={24}
-            height={24}
-            className="text-gray-500 ml-auto group-hover:text-gray-300 transition duration-200 w-[24px] h-[24px]"
-          />
-        </button>
-      </Trigger>
-
-      <Content
-        className={cn('bg-gray-700 border-t-0 border-gray-800 p-0 w-[295px] h-40 overflow-auto z-50', {
-          'rounded-t-none ': opened,
-        })}
-      >
-        <Item
-          className="group flex p-0 rounded-none hover:bg-gray-600 hover:outline-none w-full cursor-pointer"
-          onClick={() => onSelect('')}
-        >
-          <div className="flex items-center h-11 gap-[12px] px-[16px] py-[10px] text-[14px] text-gray-300 group-hover:text-white w-full">
-            <p>AI Choice</p>
-          </div>
-        </Item>
-        {agents
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map(({ id, name }) => (
-            <Item
-              key={id}
-              onClick={() => onSelect(id)}
-              className="group flex p-0 rounded-none hover:bg-gray-600 hover:outline-none w-full cursor-pointer"
-            >
-              <div className="flex items-center gap-[12px] px-[16px] py-[10px] text-[14px] text-gray-300 group-hover:text-white w-full">
-                <AgentAvatar agentId={id} type="extraSmall" className="!m-0" />
-                <p>{name}</p>
-              </div>
-            </Item>
-          ))}
-      </Content>
-    </DropdownMenu>
   );
 };
 
--- frontend/src/components/editables/chat/CommandInput.tsx
+++ frontend/src/components/editables/chat/CommandInput.tsx
@@ -19,9 +19,15 @@ import Tooltip from '@/components/common/Tooltip';
 import { Icon } from '@/components/common/icons/Icon';
 import { useChatStore } from '@/store/editables/chat/useChatStore';
 import { cn } from '@/utils/common/cn';
-import { LucideIcon } from 'lucide-react';
-import React, { useEffect, useRef } from 'react';
+import { LucideIcon, X } from 'lucide-react';
+import React, { useEffect, useRef, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
+import ChatOptions from '../assets/ChatOptions';
+import { useEditablesStore } from '@/store/editables/useEditablesStore';
+import { AgentAvatar } from './AgentAvatar';
+import { useDebounceCallback } from '@mantine/hooks';
+import { ChatAPI } from '@/api/api/ChatAPI';
+import { Material } from '@/types/editables/assetTypes';
 
 interface MessageInputProps {
   actionIcon: LucideIcon | ((props: React.SVGProps<SVGSVGElement>) => JSX.Element);
@@ -32,24 +38,30 @@ interface MessageInputProps {
 
 export const CommandInput = ({ className, onSubmit, actionIcon, actionLabel }: MessageInputProps) => {
   const ActionIcon = actionIcon;
+  const [showMentions, setShowMentions] = useState(false);
+  const [selectedAgentId, setSelectedAgentId] = useState<string>('');
   const command = useChatStore((state) => state.commandHistory[state.commandIndex]);
 
   const setCommand = useChatStore((state) => state.editCommand);
   const promptUp = useChatStore((state) => state.historyUp);
   const promptDown = useChatStore((state) => state.historyDown);
   const chat = useChatStore((state) => state.chat);
-
+  const agents = useEditablesStore((state) => state.agents);
+  const setChat = useChatStore((state) => state.setChat);
+  const materials = useEditablesStore((state) => state.materials);
+  const [materialsOptions, setMaterialsOptions] = useState<Material[]>([]);
+  const [chosenMaterials, setChosenMaterials] = useState<Material[]>([]);
+  const materialsIds = chosenMaterials.map((material) => material.id);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setCommand(e.target.value);
+    const mentionMatch = e.target.value.match(/@\w*$/);
+    setShowMentions(!!mentionMatch);
   };
 
-  const isEmptyInput = !(command.trim().length === 0 && chat?.message_groups.length === 0);
-
   const handleKeyDown = async (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === 'Enter' && !e.shiftKey && isEmptyInput) {
-      console.log('command run');
+    if (e.key === 'Enter' && !e.shiftKey) {
       e.preventDefault();
 
       await handleSendMessage();
@@ -82,27 +94,139 @@ export const CommandInput = ({ className, onSubmit, actionIcon, actionLabel }: M
     }
   }, [chat?.id]);
 
+  useEffect(() => {
+    setSelectedAgentId('');
+    setChosenMaterials([]);
+  }, [chat?.id]);
+
+  useEffect(() => {
+    if (chat?.chat_options.agent_id) {
+      setSelectedAgentId(chat?.chat_options.agent_id);
+    }
+  }, [chat?.chat_options.agent_id, chat?.id]);
+
+  useEffect(() => {
+    const filteredMaterials = materials?.filter(({ id }) => (chat?.chat_options.materials_ids || []).includes(id));
+    if (filteredMaterials) {
+      setChosenMaterials(filteredMaterials);
+    }
+  }, [chat?.chat_options.materials_ids, materials, chat?.id]);
+
+  useEffect(() => {
+    setMaterialsOptions(materials?.filter((material) => !chosenMaterials.includes(material)) as Material[]);
+  }, [chat?.id, materials, chosenMaterials]);
+
+  const getAgent = (agentId: string) => agents.find((agent) => agent.id === agentId);
+
+  const debounceChatUpdate = useDebounceCallback(async () => {
+    try {
+      if (chat) {
+        ChatAPI.patchChatOptions(chat?.id, {
+          agent_id: selectedAgentId,
+          materials_ids: materialsIds,
+          let_ai_add_extra_materials: false,
+        });
+
+        setChat({
+          ...chat,
+          chat_options: {
+            agent_id: selectedAgentId,
+            materials_ids: materialsIds,
+            let_ai_add_extra_materials: false,
+          },
+        });
+      }
+    } catch (error) {
+      console.error('An error occurred while updating chat options:', error);
+    }
+  }, 500);
+
+  const onSelectAgentId = (id: string) => {
+    setSelectedAgentId(id);
+    debounceChatUpdate();
+  };
+
+  const removeAgentId = () => {
+    setSelectedAgentId('');
+    debounceChatUpdate();
+  };
+
+  const handleMaterialSelect = (material: Material) => {
+    setChosenMaterials((prev) => [...prev, material]);
+    const filteredOptions = materialsOptions.filter(({ id }) => id !== material.id);
+    setMaterialsOptions(filteredOptions);
+    debounceChatUpdate();
+  };
+
+  const removeSelectedMaterial = (id: string) => () => {
+    const material = chosenMaterials.find((material) => material.id === id) as Material;
+    setChosenMaterials((prev) => prev.filter(({ id }) => id !== material.id));
+    setMaterialsOptions((prev) => [...prev, material].sort((a, b) => a.name.localeCompare(b.name)));
+    debounceChatUpdate();
+  };
+
   return (
     <div className={cn(className, 'flex w-full flex-col px-4 py-[20px]  bg-gray-900 z-50 ')}>
-      <div className="flex items-end gap-[10px] max-w-[700px] w-full mx-auto">
-        <TextareaAutosize
-          ref={textAreaRef}
-          className="max-h-[200px] overflow-y-auto border border-gray-500 placeholder:text-gray-400 bg-gray-800 text-[15px] text-white flex-grow resize-none overflow-hidden rounded-[8px]  px-[20px] py-[12px] hover:bg-gray-600 hover:placeholder:text-gray-300 focus:bg-gray-600 focus:border-gray-400 focus:outline-none transition duration-100"
-          value={command}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          placeholder="Type a new command here..."
-          rows={1}
-        />
+      <div className="flex items-end gap-[10px] max-w-[700px] w-full mx-auto relative">
+        {showMentions && (
+          <ChatOptions
+            onSelectAgentId={onSelectAgentId}
+            handleMaterialSelect={handleMaterialSelect}
+            materialsOptions={materialsOptions}
+          />
+        )}
+        <div className="w-full max-h-[200px] overflow-y-auto border border-gray-500 bg-gray-800 hover:bg-gray-600 focus-within:bg-gray-600 focus-within:border-gray-400 transition duration-100 rounded-[8px] flex flex-col flex-grow resize-none">
+          {(selectedAgentId || chosenMaterials.length > 0) && (
+            <div className="px-[20px] py-[12px] w-full flex flex-col gap-2">
+              {selectedAgentId && (
+                <div className="w-full flex jusify-between items-center">
+                  <div className="flex items-center gap-3 w-full">
+                    <AgentAvatar agentId={selectedAgentId} title="test" type="extraSmall" className="mb-0 mt-0" />
+                    <p>
+                      Talking to <span className="text-white">{getAgent(selectedAgentId)?.name}</span>
+                    </p>
+                  </div>
+                  <Icon
+                    icon={X}
+                    className={cn('w-4 h-4 min-h-4 min-w-4 flex-shrink-0 cursor-pointer')}
+                    onClick={removeAgentId}
+                  />
+                </div>
+              )}
+              {chosenMaterials.length > 0 && (
+                <div className="flex gap-2">
+                  <span className="text-gray-400">Using:</span>
+                  <div className="flex flex-wrap items-center gap-2 overflow-y-auto max-h-[56px]">
+                    {chosenMaterials.map((material) => (
+                      <div key={material.id} className="flex gap-1 items-center">
+                        <span className="text-gray-300">{material.name}</span>
+                        <Icon
+                          icon={X}
+                          className={cn('w-4 h-4 min-h-4 min-w-4 flex-shrink-0 cursor-pointer text-gray-400')}
+                          onClick={removeSelectedMaterial(material.id)}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
+
+          <TextareaAutosize
+            ref={textAreaRef}
+            className="w-full bg-transparent text-[15px] text-white resize-none overflow-hidden px-[20px] py-[12px] placeholder:text-gray-400 hover:placeholder:text-gray-300 focus:outline-none"
+            value={command}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            placeholder={`Type "@" to select a specific agent and materials`}
+            rows={1}
+          />
+        </div>
+
         <Tooltip label={actionLabel} position="top" align="center" sideOffset={10} disableAnimation withArrow>
           <div>
-            <Button
-              variant="primary"
-              iconOnly={true}
-              onClick={handleSendMessage}
-              classNames={cn('p-[12px]', {})}
-              disabled={!isEmptyInput}
-            >
+            <Button variant="primary" iconOnly={true} onClick={handleSendMessage} classNames={cn('p-[12px]', {})}>
               <Icon icon={ActionIcon} width={24} height={24} className="w-6 h-6" />
             </Button>
           </div>
