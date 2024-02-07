c0b0f4f65fb5e9d8a2ea9eda16ffc1dcee2d03d6
Maciej Cielecki <maciej.cielecki@10clouds.com>
2024-02-07 07:25:52 +0100

    Proper ActorId in analysis

--- backend/aiconsole/core/chat/execution_modes/analysis/gpt_analysis_function_step.py
+++ backend/aiconsole/core/chat/execution_modes/analysis/gpt_analysis_function_step.py
@@ -215,7 +215,7 @@ async def gpt_analysis_function_step(
                             await chat_mutator.mutate(
                                 SetActorIdMessageGroupMutation(
                                     message_group_id=message_group_id,
-                                    actor_id=arguments_dict["agent_id"],
+                                    actor_id=ActorId(type="agent", id=arguments_dict["agent_id"]),
                                 )
                             )
 
