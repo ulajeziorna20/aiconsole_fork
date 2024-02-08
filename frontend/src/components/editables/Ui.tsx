ce25b7e9be15ed0660b479e3ae41c3cd17fd846e
Jakub Bilko <jakub.bilko@10clouds.com>
2024-02-08 21:25:45 +0800

    feat: test uio

--- frontend/src/components/editables/Ui.tsx
+++ frontend/src/components/editables/Ui.tsx
@@ -1,17 +1,25 @@
 import { createSandbox } from '@/utils/transpilation/createSandbox';
 import { transpileCode } from '@/utils/transpilation/transpileCode';
-import { useEffect, useState, ReactNode } from 'react';
+import { useState, ReactNode } from 'react';
+import { CodeInput } from './assets/CodeInput';
+import { Button } from '../common/Button';
+import { FormGroup } from '../common/FormGroup';
 
-const code = `const Component = () => {
-  return <h2>Hi, I am a Car! {window.document.title}</h2>;
+const exampleCode = `const Component = () => {
+  return <h2>Hi, I am a Component!</h2>;
 }
 `;
 
 export const UI = () => {
   const [result, setResult] = useState<ReactNode | null>(null);
+  const [code, setCode] = useState(exampleCode);
+  const [error, setError] = useState('');
 
-  useEffect(() => {
-    const transpile = async () => {
+  const handleCodeChange = (code: string) => setCode(code);
+
+  const handleSubmit = async () => {
+    setError('');
+    try {
       const finalCode = await transpileCode(code);
 
       const sandboxed = createSandbox(finalCode);
@@ -19,10 +27,19 @@ export const UI = () => {
       if (sandboxed) {
         setResult(sandboxed());
       }
-    };
+    } catch (e) {
+      setError((e as Error).message);
+    }
+  };
 
-    transpile();
-  }, []);
-
-  return <div>{result ? result : 'ui'}</div>;
+  return (
+    <FormGroup className="flex flex-col w-full gap-[20px]">
+      <CodeInput value={code} codeLanguage="javascript" onChange={handleCodeChange} label="React Code" />
+      <Button onClick={handleSubmit} variant="secondary">
+        Transpile
+      </Button>
+      {!!error && <div className="text-danger flex text-xs">Error: {error}</div>}
+      {result}
+    </FormGroup>
+  );
 };
