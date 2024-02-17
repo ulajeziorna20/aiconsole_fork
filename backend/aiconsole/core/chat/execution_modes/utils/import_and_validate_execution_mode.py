116ca88051d296bbd118dfbce4707b00052e9c3e
vlad <vlad@malinovski.org>
2024-02-17 10:07:20 +0100

    * feat(import_and_validate_execution_mode.py): add support for porting 2.9 agent to 2.11

--- backend/aiconsole/core/chat/execution_modes/utils/import_and_validate_execution_mode.py
+++ backend/aiconsole/core/chat/execution_modes/utils/import_and_validate_execution_mode.py
@@ -7,6 +7,14 @@ from aiconsole.core.chat.execution_modes.execution_mode import ExecutionMode
 async def import_and_validate_execution_mode(agent: AICAgent):
     execution_mode = agent.execution_mode
 
+    # Port 2.9 agent to 2.11
+    if execution_mode == "aiconsole.core.execution_modes.normal:execution_mode_normal":
+        execution_mode = "aiconsole.core.chat.execution_modes.normal:execution_mode"
+    elif execution_mode == "aiconsole.core.execution_modes.interpreter:execution_mode_interpreter":
+        execution_mode = "aiconsole.core.chat.execution_modes.interpreter:execution_mode"
+    elif execution_mode == "aiconsole.core.execution_modes.example_countdown:execution_mode_example_countdown":
+        execution_mode = "aiconsole.core.chat.execution_modes.example_countdown:execution_mode"
+
     split = execution_mode.split(":")
 
     if len(split) != 2:
